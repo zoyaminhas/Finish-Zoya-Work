39924391f73bd86a48415b5905bef015b3bc9c0a
Zoya Minhas <zoyaminhas100@outlook.com>
2023-04-08 12:41:02 +0500

    Commit

---
+++ FirstAzureAppTestProject/CrossBrowserTest_by_SeleniumIntegration.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Safari;
+ 
+
+namespace MyFirstAzureWebApp.Pages.Tests
+{
+    /**
+    * This class demonstrate running tests against web browsers
+    * <p>
+    * Notes :
+    * Switching between the browsers could be transferred to a Base Class.
+    * Please refer to https://git.io/fhqYE  for more concise version
+    */
+  //  [TestClass]
+    public class CrossBrowserTest_by_SeleniumIntegration
+    {
+        private static String BASE_URL = "http://hrm.pragmatictestlabs.com";
+        private static String USERNAME = "Admin";
+        private static String PASSWORD = "Ptl@#321";
+        private static String WELCOME_MESSAGE = "Welcome Admin";
+        private static int TIMEOUT = 30;
+
+
+        private void PerformTask(IWebDriver driver)
+        {
+
+            //Set the implicit wait
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(TIMEOUT);
+
+            driver.Manage().Window.FullScreen();
+            driver.Navigate().GoToUrl(BASE_URL);
+
+            driver.FindElement(By.Name("txtUsername")).SendKeys(USERNAME);
+            driver.FindElement(By.Name("txtPassword")).SendKeys(PASSWORD);
+            driver.FindElement(By.Name("txtPassword")).SendKeys(Keys.Return);
+
+            String welcomeMessage = driver.FindElement(By.Id("welcome")).Text;
+
+            Assert.AreEqual(welcomeMessage, WELCOME_MESSAGE, "Welcome message is incorrect ");
+            driver.Quit();
+        }
+
+
+        /**
+        * Demonstrate launching Google Chrome web browser
+        */
+        // [TestMethod]
+        public void OpenChrome()
+        {
+            IWebDriver driver = new ChromeDriver();
+            PerformTask(driver);
+        }
+
+        /**
+        * This method demonstrate launching Chrome headless mode
+        * You can create an instance of ChromeOptions,
+        * which has convenient methods for setting ChromeDriver-specific capabilities.
+        * You can then pass the ChromeOptions object into the ChromeDriver constructor:
+        * Please refer to http://chromedriver.chromium.org/capabilities
+        */
+        // [TestMethod]
+        public void OpenChromeHeadless()
+        {
+            var options = new ChromeOptions();
+            options.AddArguments("disable-infobars");
+            options.AddArguments("headless");
+            IWebDriver driver = new ChromeDriver(options);
+            PerformTask(driver);
+        }
+        /**
+       * This method demonstrate launching Firefox web browser
+       */
+        // [TestMethod]
+        public void OpenFirefox()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            PerformTask(driver);
+        }
+
+        /**
+        * This method demonstrate launching Firefox headless mode
+        */
+        // [TestMethod]
+        public void OpenFirefoxHeadless()
+        {
+            var options = new FirefoxOptions();
+            options.AddArguments("headless");
+            IWebDriver driver = new FirefoxDriver(options);
+            PerformTask(driver);
+        }
+
+        /**
+        * This method demonstrate launching IE web browser
+        */
+        // [TestMethod]
+        public void OpenIE()
+        {
+            InternetExplorerOptions options = new InternetExplorerOptions();
+            options.IgnoreZoomLevel = true; //Ignoring the Zoom level setting 
+            //options.FlakinessByIgnoringSecurityDomains(); //Ignoring the Security domains settings 
+            options.EnableNativeEvents = false; //Disabling the NATIVE_EVENTS capability to ensure the typing speed with 64bit driver 
+                                           //Launch IE browser
+            var driver = new InternetExplorerDriver(options);
+            PerformTask(driver);
+        }
+
+        /**
+        * This method demonstrate launching Edge web browser
+        */
+        // [TestMethod]
+        public void OpenEdge()
+        {
+            IWebDriver driver = new EdgeDriver();
+            PerformTask(driver);
+        }
+
+        ///**
+        //* This method demonstrate launching Opera  web browser
+        //*/
+        //// [TestMethod]
+        //public void OpenOpera()
+        //{
+        //    OperaOptions options = new OperaOptions();
+        //    options.BinaryLocation = "/Applications/Opera.app/Contents/MacOS/Opera";
+
+        //    var driver = new OperaDriver(options);
+        //    PerformTask(driver);
+
+        //}
+
+        /**
+         * Demonstrate launching Safari web browser
+         * <p>
+         * Note :
+         * Browser driver configuration is not required as browser has the driver inbuilt
+         */
+        // [TestMethod]
+        public void OpenSafari()
+        {
+            var driver = new SafariDriver();
+            PerformTask(driver);
+
+        }
+
+        /**
+          * Demonstrate launching Chrome Mobile Emulator
+          */
+        // [TestMethod]
+        public void OpenMobileEmulation()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.EnableMobileEmulation("iPhone X");
+            //chromeOptions.EnableMobileEmulation("Google Nexus 5");
+            IWebDriver driver = new ChromeDriver(chromeOptions);
+ 
+            PerformTask(driver);
+        }
+    }
+
+}
---
+++ FirstAzureAppTestProject/LoginTest_by_SeleniumIntegration.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace MyFirstAzureWebApp.Pages.Tests
+{
+   // [TestClass]
+    public class LoginTest_by_SeleniumIntegration
+    {
+
+        private IWebDriver driver;
+        private static String BASE_URL = "http://hrm.pragmatictestlabs.com";
+
+        [TestInitialize()]
+        public void BeforeMethod()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("disable-infobars");
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.FullScreen();
+            driver.Navigate().GoToUrl(BASE_URL);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [TestCleanup()]
+        public void afterMethod()
+        {
+            driver.Close();
+        }
+
+
+        // [TestMethod]
+        public void testLoginWithValidCredentials()
+        {
+
+            //<input name="txtUsername" id="txtUsername" type="text" value="Admin">
+            driver.FindElement(By.Id("txtUsername")).SendKeys("Admin");
+
+
+            //Check if password input is of type="password"
+            Assert.AreEqual(driver.FindElement(By.Id("txtPassword")).GetAttribute("type"), "password");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("Ptl@#321");
+
+
+            Assert.AreEqual(driver.FindElement(By.Id("txtUsername")).GetAttribute("value"), "Admin");
+
+            driver.FindElement(By.Id("btnLogin")).Click();
+            String strWelcomeMessage = driver.FindElement(By.Id("welcome")).Text;
+            Assert.AreEqual(strWelcomeMessage, "Welcome Admin");
+        }
+
+
+        // [TestMethod]
+        public void testValidUserLoginUsingKeyboardKeys()
+        {
+            driver.FindElement(By.Id("txtUsername")).SendKeys("Admin");
+            driver.FindElement(By.Id("txtUsername")).SendKeys(Keys.Tab);
+
+            driver.FindElement(By.Id("txtPassword")).SendKeys("Ptl@#321");
+            driver.FindElement(By.Id("txtPassword")).SendKeys(Keys.Return);
+
+
+            String strWelcomeMessage = driver.FindElement(By.Id("welcome")).Text;
+            Assert.AreEqual(strWelcomeMessage, "Welcome Admin");
+        }
+
+        // [TestMethod]
+        public void testLoginWithInvalidPassword()
+        {
+            driver.FindElement(By.Id("txtUsername")).SendKeys("Admin");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("InvalidPWd");
+            driver.FindElement(By.Id("btnLogin")).Click();
+
+            ///<span id="spanMessage">Invalid credentials</span>
+
+            String msgError = driver.FindElement(By.Id("spanMessage")).Text;
+            Assert.AreEqual(msgError, "Invalid credentials");
+        }
+
+        // [TestMethod]
+        public void testLoginWithBlankUsername()
+        {
+            driver.FindElement(By.Id("txtUsername")).SendKeys("");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("Ptl@#321");
+            driver.FindElement(By.Id("btnLogin")).Click();
+
+            String msgError = driver.FindElement(By.Id("spanMessage")).Text;
+            Assert.AreEqual(msgError, "Username cannot be empty");
+        }
+
+
+        // [TestMethod]
+        public void testLoginWithBlankUsernameAndPassword()
+        {
+            driver.FindElement(By.Id("txtUsername")).SendKeys("");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("");
+            driver.FindElement(By.Id("btnLogin")).Click();
+
+            String msgError = driver.FindElement(By.Id("spanMessage")).Text;
+            Assert.AreEqual(msgError, "Username cannot be empty");
+
+        }
+
+        // [TestMethod]
+        public void testLoginWithBlankPassword()
+        {
+            driver.FindElement(By.Id("txtUsername")).SendKeys("Admin");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("");
+            driver.FindElement(By.Id("btnLogin")).Click();
+
+            String msgError = driver.FindElement(By.Id("spanMessage")).Text;
+            Assert.AreEqual(msgError, "Password cannot be empty");
+
+        }
+
+
+        // [TestMethod]
+        public void testCaseSensitivityOfPassword()
+        {
+            driver.FindElement(By.Id("txtUsername")).SendKeys("Admin");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("ptl@#321");
+            driver.FindElement(By.Id("btnLogin")).Click();
+
+            String msgError = driver.FindElement(By.Id("spanMessage")).Text;
+            Assert.AreEqual(msgError, "Invalid credentials");
+        }
+
+
+        // [TestMethod]
+        public void testLogoutFromSystem()
+        {
+            driver.FindElement(By.Id("txtUsername")).SendKeys("Admin");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("Ptl@#321");
+            driver.FindElement(By.Id("btnLogin")).Click();
+            driver.FindElement(By.Id("welcome")).Click();
+            driver.FindElement(By.LinkText("Logout")).Click();
+
+            String currentURL = driver.Url;
+            Assert.AreEqual(currentURL, "http://hrm.pragmatictestlabs.com/symfony/web/index.php/auth/login");
+            driver.Navigate().Back();
+            Assert.IsFalse(driver.FindElement(By.Id("welcome")).Text.Length > 0);
+
+        }
+    }
+
+}
